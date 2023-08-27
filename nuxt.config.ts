5481310e48ef2158b609192409e1f00c482724af
Egor Chebotarev <eg.chebotarev@gmail.com>
2023-08-27 18:49:25 +0100

    Vuetify: treeshaking

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
+import vuetify from "vite-plugin-vuetify";
+import { HookResult } from "@nuxt/schema";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
   devtools: { enabled: true },
   css: ["~/assets/css/main.css", "vuetify/lib/styles/main.sass"],
   build: {
@@ -10,7 +14,15 @@ export default defineNuxtConfig({
       "process.env.DEBUG": false,
     },
   },
-  modules: ["@nuxt/content"],
+  modules: [
+    "@nuxt/content",
+    async (options, nuxt) => {
+      nuxt.hooks.hook(
+        "vite:extendConfig",
+        (config) => config?.plugins?.push(vuetify()) as HookResult,
+      );
+    },
+  ],
   postcss: {
     plugins: {
       tailwindcss: {},

f2e0cd0edde6dd2a0c1bcbca36ab13d4fc488ceb
Egor Chebotarev <eg.chebotarev@gmail.com>
2023-09-03 18:28:58 +0100

    Запуск видео

--- components/common/VideoContainer.vue
+++ components/common/VideoContainer.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
 interface Props {
   src: string;
-  controls: boolean;
-  width: number | string;
-  height: number | string;
+  poster?: string;
+  controls?: boolean;
+  width?: number | string;
+  height?: number | string;
 }
 const props = withDefaults(defineProps<Props>(), {
   src: "",
@@ -11,18 +12,42 @@ const props = withDefaults(defineProps<Props>(), {
   width: "100%",
   height: "100%",
 });
+
+const video = ref<HTMLIFrameElement | null>(null);
+
+const isShow = ref(false);
+const runVideo = () => {
+  isShow.value = true;
+  video.play();
+};
 </script>
 
 <template>
-  <iframe
-    :width="props.width"
-    :height="props.height"
-    :src="`${props.src}${!props.controls ? ';controls=0' : ''}`"
-    title="YouTube video player"
-    frameborder="0"
-    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-    allowfullscreen
-  ></iframe>
+  <div @click="runVideo">
+    <div v-if="!isShow && props.poster">
+      <img :src="props.poster" alt="" />
+      <img
+        src="/images/play.svg"
+        alt=""
+        class="play absolute bottom-0 right-2"
+      />
+    </div>
+    <iframe
+      v-else
+      ref="video"
+      :width="props.width"
+      :height="props.height"
+      :src="`${props.src}${!props.controls ? ';controls=0' : ''}`"
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allowfullscreen
+    ></iframe>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.play {
+  transform: rotate(-3deg);
+}
+</style>
